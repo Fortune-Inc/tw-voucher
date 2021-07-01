58f5418c9915403848f561f2afc154832cacbe8f
Chelos <chan022@outlook.com>
2021-07-02 01:25:21 +0700

    *More Compact*

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const f = require("petitio");
-module.exports = async (e, t) => {
-    const o = (t = t.trim()).match(/(?<=\?v=)([0-9A-Za-z])+/);
-    if (!(t = o ? o[0] : t) || 18 > t.length) throw Error("INVAILD_VOUCHER_CODE");
-    const a = await f(`https://gift.truemoney.com/campaign/vouchers/${t}/redeem`, "POST").body({
-        mobile: e,
-        voucher_hash: t
-    }).json();
-    if ("SUCCESS" === a.status.code) return {
-        amount: parseFloat(a.data.voucher.redeemed_amount_baht),
-        owner_full_name: a.data.owner_profile.full_name
-    };
-    throw Error(a.status.code)
-};
+module.exports=async(e,t)=>{let r=(t=t.trim()).match(/(?<=\?v=)([0-9A-Za-z])+/);if(!(t=r?r[0]:t)||18>t.length)throw Error("INVAILD_VOUCHER_CODE");let o=await require("petitio")(`https://gift.truemoney.com/campaign/vouchers/${t}/redeem`,"POST").body({mobile:e,voucher_hash:t}).json();if("SUCCESS"===o.status.code)return{amount:Number(o.data.voucher.redeemed_amount_baht),owner_full_name:o.data.owner_profile.full_name};throw Error(o.status.code)};
