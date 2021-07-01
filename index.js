04ce59096f51d0fdb993c48231bc6c3899713d91
Chelos <chan022@outlook.com>
2021-07-01 23:53:45 +0700

    More Compact

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const f = require('petitio')
-
-module.exports = async (phone, code) => {
-  code = code.trim()
-  const matched = code.match(/(?<=\?v=)([0-9A-Za-z])+/)
-  code = matched ? matched[0] : code
-  if (!code || code.length < 18) {
-    throw new Error('INVAILD_VOUCHER_CODE')
-  }
-  const res = await f(`https://gift.truemoney.com/campaign/vouchers/${code}/redeem`, 'POST').body({ mobile: phone, voucher_hash: code }).json()
-  if (res.status.code === 'SUCCESS' && res.data && res.data.voucher) {
-    return {
-      amount: parseFloat(res.data.voucher.redeemed_amount_baht),
-      owner_full_name: res.data.owner_profile.full_name
-    }
-  } else {
-    throw new Error(res.status.code)
-  }
-}
+const f = require("petitio");
+module.exports = async (e, t) => {
+    const o = (t = t.trim()).match(/(?<=\?v=)([0-9A-Za-z])+/);
+    if (!(t = o ? o[0] : t) || 18 > t.length) throw Error("INVAILD_VOUCHER_CODE");
+    const a = await f(`https://gift.truemoney.com/campaign/vouchers/${t}/redeem`, "POST").body({
+        mobile: e,
+        voucher_hash: t
+    }).json();
+    if ("SUCCESS" === a.status.code) return {
+        amount: parseFloat(a.data.voucher.redeemed_amount_baht),
+        owner_full_name: a.data.owner_profile.full_name
+    };
+    throw Error(a.status.code)
+};
