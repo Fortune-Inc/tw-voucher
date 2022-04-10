1c4b80b8f5a2ef620db46788845390acf1f9f63b
Chelos <chan022@outlook.com>
2022-04-11 00:53:31 +0700

    tls bypass fix

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports=async(e="",t="")=>{if(!(e=(e+"").trim()).length||e.match(/\D/))throw Error("INVAILD_PHONE");let r=(t+="").split("v=");if(18!=(t=(r[1]||r[0]).match(/[0-9A-Za-z]+/)[0]).length)throw Error("INVAILD_VOUCHER");let o=await require("petitio")(`https://voucher.meowcdn.xyz/api`,"POST").body({mobile:e,voucher:t}).json();if("SUCCESS"==o.status.code)return{amount:Number(o.data.my_ticket.amount_baht.replace(/,/g,'')),owner_full_name:o.data.owner_profile.full_name,code:t};throw Error(o.status.code)};
+let tls = require("tls");
+tls.DEFAULT_MIN_VERSION = "TLSv1.3";
+
+const {fetch} = require("undici")
+
+module.exports=async(e="",t="")=>{if(!(e=(e+"").trim()).length||e.match(/\D/))throw Error("INVAILD_PHONE");let r=(t+="").split("v=");if(18!=(t=(r[1]||r[0]).match(/[0-9A-Za-z]+/)[0]).length)throw Error("INVAILD_VOUCHER");let o=await fetch(`https://gift.truemoney.com/campaign/vouchers/${t}/redeem`,{method:"POST",headers:{"content-type":"application/json"},body:JSON.stringify({mobile:e,voucher_hash:t})}).then((e=>e.json()));if("SUCCESS"==o.status.code)return{amount:Number(o.data.my_ticket.amount_baht.replace(/,/g,'')),owner_full_name:o.data.owner_profile.full_name,code:t};throw Error(o.status.code)};
